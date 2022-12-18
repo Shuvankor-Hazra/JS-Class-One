ed200fbadd2853ab09f1f84b2b0b7f5b23c49bb6
Shuvankor-Hazra <shuvo.s.ovuhs@gmail.com>
2022-12-18 13:45:40 +0600

    JS Class One Compleated

---
+++ script.js
@@ -0,0 +1,32 @@
+// First part
+let life = "Beautiful";
+if (life === "Beautiful") alert("Life is Beautiful");
+// console.log(6 + 9 + 10);
+
+let firstname = "Asif";
+
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+
+let lastname = "Rahim";
+
+console.log(lastname);
+console.log(lastname);
+console.log(lastname);
+console.log(lastname);
+console.log(lastname);
+
+let last_name = "Shuvo";
+
+console.log(last_name);
+console.log(last_name);
+console.log(last_name);
+console.log(last_name);
+console.log(last_name);
+
+console.log(100000);
+
+console.log("I Love Bangladesh");
